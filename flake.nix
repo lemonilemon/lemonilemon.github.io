45005aac53cb0fb311bc3d063694d845d000d7da
lemonilemon <imlemonilemon@gmail.com>
2025-06-30 15:48:04 +0800

    add nix flake for my personal usage

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Hugo blog project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          hugo
+          nodejs
+          go
+        ];
+      };
+
+      packages.${system}.default = pkgs.hugo;
+    };
+}
